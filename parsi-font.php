e23aa8a6feccbfeed58d5d2c953abe79968e7791
Shahrakdownload <Shahrakdownload@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-27 18:07:18 +0000

    Commit Parsi Font to http://plugins.svn.wordpress.org/parsi-font
    git-svn-id: https://plugins.svn.wordpress.org/parsi-font/trunk@866297 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- parsi-font.php
+++ parsi-font.php
@@ -4,7 +4,7 @@ Plugin Name: WP-Parsi Admin Font Editor
 Plugin URI: http://www.forum.wp-parsi.com/
 Description: Change fonts of wordpress.
 Author: Ghaem
-Version: 3.5
+Version: 3.0
 Tags: admin, admin font, html editor, visual editor, fonts, font, wordpress font, change font, parsi font, fonts, oersian, persian fonts, persian font
 Author URI: http://www.forum.wp-parsi.com/
 Copyright: Ghaem Omidi(WP-Parsi)
